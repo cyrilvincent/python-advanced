c36e757bf69c6d15680b060ff300981ed0b9a9d5
cyrilvincent <contact@cyrilvincent.com>
2020-06-14 19:34:53 +0200

    ML

--- demoreduce.py
+++
@@ -1,5 +0,0 @@
-import functools
-l = range(1,10)
-
-res = functools.reduce(lambda acc,cur : acc + cur, l)
-print(res)
--- demosql.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect("mesures/mesures.db3")
-c = db.cursor()
-c.execute("select * from mesure")
-for row in c:
-    print(row[4])
