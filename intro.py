1c34ef57e17087c990689365650f434dea34f3df
cyrilvincent <contact@cyrilvincent.com>
2020-06-15 11:15:18 +0200

    ST 0.25/3

--- intro.py
+++ intro.py
@@ -4,5 +4,12 @@ def isEven(x):
     return x % 2 == 0
 
 def isPrime(x):
-    pass
+    if x < 2:
+        return False
+    else:
+        for div in range(2,x):
+            if x % div == 0:
+                return False
+        return True
+
 
