8ab8e196d0ed75389273d35046d9e3f539899dca
cyrilvincent <contact@cyrilvincent.com>
2020-06-16 10:41:55 +0200

    ST 1.25/3

--- geometry.py
+++ geometry.py
@@ -1,5 +1,20 @@
+import unittest
+
 class Rectangle:
 
-    pass
-# 2 attributs
-# 2 méthodes : computeArray, computePerimeter
+    def __init__(self, length = 0, width = 0):
+        self.length = length
+        self.width = width
+
+    def getArea(self):
+        return self.length * self.width
+
+    def getPerimeter(self):
+        return 2 * (self.length + self.width)
+
+class GeometryTest(unittest.TestCase):
+
+    def testRectangle(self):
+        r1 = Rectangle(3,2)
+        self.assertEqual(6, r1.getArea())
+        self.assertEqual(10, r1.getPerimeter())
--- media.py
+++ media.py
@@ -1,5 +1,24 @@
+import datetime
+import unittest
+
 class Book:
 
-    pass
-# Environ 7-8 attributs
-# 1 méthode : NetPrice 5.5% "en dur" price * 1.055
+    def __init__(self, isbn, title, price, author, editor = None, publicationDate = datetime.datetime.now(), category=None):
+        self.isbn = isbn
+        self.title = title
+        self.price = price
+        self.author = author
+        self.editor = editor
+        self.publicationDate = publicationDate
+        self.category = category
+
+    def getNetPrice(self):
+        return self.price * 1.055
+
+class MediaTest(unittest.TestCase):
+
+    def testBook(self):
+        b1 = Book("123","Python",10,"Cyril")
+        netPrice = b1.getNetPrice()
+        self.assertAlmostEqual(10.55, netPrice,delta=1e-3)
+        b2 = Book("123", "Python", 10, "Cyril", publicationDate=datetime.datetime(2020,6,16))
