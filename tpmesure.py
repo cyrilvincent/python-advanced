901cf3b99d1452820991a4b9f323a8c57bcfc383
cyrilvincent <contact@cyrilvincent.com>
2020-06-17 15:54:21 +0200

    ST 3/3

--- tpmesure.py
+++ tpmesure.py
@@ -3,3 +3,15 @@
 # |VM - VT| > bruit, bruit = 1v
 # Filtrer le dataframe pour obtenir les erreurs
 # Bonus afficher à l'écran
+# 15h45
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+dataframe = pd.read_csv("data/mesures/mesures.csv")
+plt.plot(dataframe["T"], dataframe.VM)
+errors = np.abs(dataframe.VM - dataframe.VT)
+plt.plot(dataframe["T"], errors)
+plt.show()
+errors = dataframe[errors > 1]
+print(errors)
