ce64402cdbe36ecac51e7937f1a4093837c28f36
cyrilvincent <contact@cyrilvincent.com>
2020-06-17 15:20:28 +0200

    ST 3/3

---
+++ tpmesure.py
@@ -0,0 +1,5 @@
+# Charger avec pandas mesures.csv
+# Calculer la moyenne de VM => 0 le max = 240
+# |VM - VT| > bruit, bruit = 1v
+# Filtrer le dataframe pour obtenir les erreurs
+# Bonus afficher à l'écran
