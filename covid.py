5ef69de07a9b561652ae2a105634afd2661f5d64
cyrilvincent <contact@cyrilvincent.com>
2020-06-17 13:56:22 +0200

    ST 2.75/3

--- covid.py
+++ covid.py
@@ -11,3 +11,9 @@ plt.yscale("log")
 plt.scatter(x, ynbcas)
 plt.bar(x, ydc)
 plt.show()
+
+# transformer les listes python en x = np.array(list)
+# nbcas et dc = moyenne, min, max
+# x = x[x > 40]
+# y = y[x > 40] recalculer moyenne, min max au delà du 40ème jour
+# tauxletalites = dc / nbcas par jour, moyenne
