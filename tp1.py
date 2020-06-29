ecb6aec7d57eb9ce1db1f494e5c3b0ee005a53d3
cyrilvincent <contact@cyrilvincent.com>
2020-06-29 11:33:31 +0200

    ST 0.25/3

--- tp1.py
+++ tp1.py
@@ -1,5 +1,17 @@
 import unittest
 
+def isEven(x: int) -> bool:
+    return x % 2 == 0
+
+def isPrime(x: int) -> bool:
+    if x < 2:
+        return False
+    else:
+        for div in range(2,x):
+            if x % div == 0:
+                return False
+        return True
+
 class TP1Test(unittest.TestCase):
 
     def testIsEven(self):
@@ -10,3 +22,4 @@ class TP1Test(unittest.TestCase):
         self.assertTrue(isPrime(7))
         self.assertFalse(isPrime(8))
         self.assertTrue(isPrime(2))
+        self.assertTrue(isPrime(524287))
