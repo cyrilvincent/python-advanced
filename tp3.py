f22cd43ff3d9579327e2c3648ce27adb4dad66a3
cyrilvincent <contact@cyrilvincent.com>
2020-06-29 15:07:19 +0200

    ST 0.75/3

--- tp3.py
+++ tp3.py
@@ -1,5 +1,19 @@
 l = [1,5,99,-8,2,7,45]
 
+def min_max_avg(l):
+    min = l[0]
+    max = l[0]
+    sum = l[0]
+    nb = 1
+    for val in l[1:]:
+        if val < min:
+            min = val
+        elif val > max:
+            max = val
+        sum += val
+        nb += 1
+    return min, max, sum / nb
+
 import unittest
 
 class TP3Test(unittest.TestCase):
@@ -8,4 +22,4 @@ class TP3Test(unittest.TestCase):
         min, max, avg = min_max_avg(l)
         self.assertEqual(min, -8)
         self.assertEqual(max, 99)
-        self.assertEqual(avg, 50.0)
+        self.assertAlmostEqual(avg, 21.571, delta = 1e-3)
