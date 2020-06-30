a89869e3813373cd0ced78980c7624456c24b8d5
cyrilvincent <contact@cyrilvincent.com>
2020-06-30 09:17:53 +0200

    ST 1/3

--- media.py
+++ media.py
@@ -1,7 +1,21 @@
+import datetime
+
 class Book:
-    pass
-    #Quoi?
-    #5-7 attributs
-    #Coder + Tester unittest
-    #Comment?
-    #getNetPrice() => price * 1.055
+
+    def __init__(self, id, title, price, author=None, nbPage=0, date=datetime.datetime.now()):
+        self.id = id
+        self.title = title
+        self.price = price
+        self.author = author
+        self.nbPage = nbPage
+        self.date = date
+
+    def getNetPrice(self):
+        return self.price * 1.055
+
+import unittest
+class MediaTest(unittest.TestCase):
+
+    def testBook(self):
+        b = Book(0,None,10)
+        self.assertAlmostEqual(10 * 1.055, b.getNetPrice(), 1e-4)
