b4ab3ef60e6a68ad68e4d5994820e2335bd57daa
cyrilvincent <contact@cyrilvincent.com>
2020-07-01 13:18:36 +0200

    ST 2.75/3

--- demomatplotlib.py
+++ demomatplotlib.py
@@ -1,11 +1,5 @@
 import matplotlib.pyplot as plt
 
-x = [0,1,2,3,4,5,6,7,8,9]
-y = [0,1,2,3,4,5,6,7,8,9]
-
-plt.bar(x,y) #plot scatter
-plt.show()
-
 import csv
 with open("data/house/house.csv") as f:
     reader = list(csv.DictReader(f))
