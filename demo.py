77e5f777c321fb3d4a23ffcdb1ff615afda2931f
cyrilvincent <contact@cyrilvincent.com>
2020-09-30 11:23:57 +0200

    ST

--- demo.py
+++ demo.py
@@ -1,5 +1,3 @@
-l = range(100)
-
 def is_prime(x):
     if x < 2:
         return False
@@ -8,3 +6,38 @@ def is_prime(x):
             if x % div == 0:
                 return False
     return True
+
+def filter_even(l):
+    res = []
+    for i in l:
+        if i % 2 == 0:
+            res.append(i)
+    return res
+
+def myfilter(fn, l):
+    res = []
+    for i in l:
+        if fn(i):
+            res.append(i)
+    return res
+
+def mymap(fn, l):
+    res = []
+    for i in l:
+        res.append(fn(i))
+    return res
+
+def double(x):
+    return x * 2
+
+def inc(x):
+    return x + 1
+
+
+
+l = range(100)
+print(filter_even(l))
+
+
+
+# Reprise 10h35
--- myfirsttests.py
+++ myfirsttests.py
@@ -1,5 +1,6 @@
 import unittest
 import demo
+import math
 
 class MyTests(unittest.TestCase):
 
@@ -9,9 +10,57 @@ class MyTests(unittest.TestCase):
         self.assertEqual(2,i)
 
     def test_prime_number(self):
-        self.assertTrue(demo.is_prime(7))
+        self.assertTrue(demo.is_prime(x=7))
         self.assertFalse(demo.is_prime(8))
         self.assertFalse(demo.is_prime(1))
+
+    def test_var_function(self):
+        fn = demo.is_prime
+        self.assertTrue(fn(7))
+
+    def test_lambda(self):
+        # f(x) = x + 1 <=>
+        f = lambda x : x + 1
+        self.assertEqual(2,f(1))
+
+    def test_filter_even(self):
+        l = [0,1,2,3]
+        res = demo.filter_even(l)
+        self.assertListEqual([0,2], res)
+
+    def test_myfilter(self):
+        l = [0,1,2,3,4,5,6,7]
+        res = demo.myfilter(demo.is_prime, l)
+        self.assertListEqual([2,3,5,7], res)
+
+    def test_filter(self):
+        l = [0,1,2,3,4,5,6,7]
+        res = filter(demo.is_prime, l)
+        res = list(res)
+        print(res)
+        self.assertListEqual([2,3,5,7], res)
+
+    def test_map(self):
+        l = [0, 1, 2, 3, 4]
+        res = demo.mymap(demo.double, l)
+        self.assertListEqual([0,2,4,6,8], res)
+        res = map(demo.double, l)
+        res = list(res)
+        self.assertListEqual([0,2,4,6,8], res)
+        res = map(lambda x : x + 1, l)
+        res = list(res)
+        self.assertListEqual([1,2,3,4,5], res)
+
+    def test_tp1(self):
+        l = range(5)
+        # Filtrer les nombres pairs
+        # Monter au carré les nombres pairs
+        self.assertListEqual([0, 4, 16], res)
+        # Obtenir la racine carrée des nombres premiers de l
+        self.assertAlmostEqual(2, math.sqrt(2) ** 2,delta=1e-5)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
