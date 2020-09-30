d9742cdbe627025827bae66b71b30940e52bb055
cyrilvincent <contact@cyrilvincent.com>
2020-09-30 08:59:53 +0200

    ST 0/3

--- demodb.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as db
-
-with db.connect("data/media/books.db3") as conn:
-    generator = conn.cursor()
-    generator.execute("select id,title,price from book")
-    for row in generator:
-        print(row[1])
--- hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello World!")
-import numpy as np
-print(np.pi)
