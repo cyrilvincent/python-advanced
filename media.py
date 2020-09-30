d3362ed45f564f39a905db0d7a2b27c31e3f7242
cyrilvincent <contact@cyrilvincent.com>
2020-09-30 16:55:16 +0200

    ST

--- media.py
+++ media.py
@@ -4,11 +4,22 @@ class Book:
 
     def __init__(self, title, price, publicationDate = datetime.datetime.now(), color = "", authors = [], nbPage = 0):
         self.title = title
-        self.price = price
+        self._price = price
         self.publicationDate = publicationDate
         self.color = color
         self.nbPage = nbPage
 
     def netPrice(self):
-        return self.price * 1.055
+        return self._price * 1.055
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            raise ValueError("Price <= 0")
+        else:
+            self._price = value
 
--- mediatest.py
+++ mediatest.py
@@ -1,10 +1,17 @@
 import unittest
 import media
 import datetime
+import logging
 
 class MediaTest(unittest.TestCase):
 
     def test_first(self):
+        logging.basicConfig(filename="log.txt")
         b1 = media.Book("Python", 10.0)
         b2 = media.Book("Numpy", 20, datetime.datetime(2020,9,30,16,8),"red",authors = ["Cyril","Toto"],nbPage=90)
         self.assertAlmostEqual(10.55, b1.netPrice(), delta=1e-3)
+        b1.price = 20
+        self.assertEqual(20, b1.price)
+        with self.assertRaises(ValueError):
+            logging.error("Price < 0")
+            b1.price = -10
