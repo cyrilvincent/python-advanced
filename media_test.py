6f8c91a8fea5b4281461b852beaaaf12a7e7ae6e
cyrilvincent <contact@cyrilvincent.com>
2020-11-02 16:31:07 +0100

    ATP

---
+++ media_test.py
@@ -0,0 +1,11 @@
+import unittest
+import media
+
+class MediaTest(unittest.TestCase):
+
+    def test_book(self):
+        # Créer le module media.py et media_test.py
+        # Créer la class Book avec 5 à 7 attributs de votre choix
+        # Créer la méthode net_price(self) * 5.5%
+        # Tester net_price
+        # Refaire tourner geometry chez vous
