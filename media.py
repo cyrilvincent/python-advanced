4ddaeeff711c40e2109b300de8a04fc089d54959
cyrilvincent <contact@cyrilvincent.com>
2020-11-03 09:19:21 +0100

    ATP

---
+++ media.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Book:
+
+    def __init__(self, isbn:str, title:str, price:float, authors:List[str], type:str="", nb_page:int=0):
+        self.isbn = isbn
+        self.title = title
+        self.price = price
+        self.authors = authors
+        self.type = type
+        self.nb_page = nb_page
+
+    def net_price(self):
+        return self.price * (1 + 0.055)
+
+
+
--- media_test.py
+++ media_test.py
@@ -4,8 +4,5 @@ import media
 class MediaTest(unittest.TestCase):
 
     def test_book(self):
-        # Créer le module media.py et media_test.py
-        # Créer la class Book avec 5 à 7 attributs de votre choix
-        # Créer la méthode net_price(self) * 5.5%
-        # Tester net_price
-        # Refaire tourner geometry chez vous
+        b1 = media.Book("123", "Python",10,["Cyril"],99)
+        self.assertAlmostEqual(10.55,b1.net_price(),delta=1e-3)
