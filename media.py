1c165ae07c7e52f6a84d2140afadadfbfed5dffa
cyrilvincent <contact@cyrilvincent.com>
2020-11-03 10:38:44 +0100

    ATP

--- media.py
+++ media.py
@@ -1,8 +1,15 @@
+from dataclasses import dataclass
 from typing import List
 
+@dataclass
 class Publisher:
 
+    name:str
+    address:str=""
+
 class Book:
+
+    tva = 0.055
 
     def __init__(self, isbn:str, title:str, price:float, authors:List[str], type:str="", nb_page:int=0):
         self.isbn = isbn
@@ -25,6 +32,12 @@ class Book:
             self._price = value
         else:
             raise ValueError("Price <= 0")
+
+    @dataclass()
+    def __del__(self):
+        pass
+        # Dernière méthode appelée avant destruction de l'objet
+
 
 
 
--- media_test.py
+++ media_test.py
@@ -16,8 +16,11 @@ class MediaTest(unittest.TestCase):
         self.assertEqual(i,j)
         j += 1
         self.assertNotEqual(i, j)
+        self.assertEqual(0, media.Book.nbBook)
         b1 = media.Book("123", "Python", 10, ["Cyril"], 99)
+        self.assertEqual(1, media.Book.nbBook)
         b2 = media.Book("123", "Python", 10, ["Cyril"], 99)
+        self.assertEqual(2, media.Book.nbBook)
         b1._price = 0
         self.assertNotEqual(b1, b2)
         self.assertIsNot(b1, b2)
@@ -26,7 +29,12 @@ class MediaTest(unittest.TestCase):
         self.assertIs(b1, b2)
         b1 = None #Rare
         del(b1) #Rare
+        self.assertEqual(1, media.Book.nbBook)
+        # Passer la TVA en static
+        # Compter automatiquement le nombre de livre instanciés
+
 
     def test_publisher(self):
         p1 = media.Publisher("ENI", "Champs Elysée, Paris")
         self.assertEqual(p1.name, "ENI")
+
