d82f181707e27c9581f257e9d0f078c790ca20fe
cyrilvincent <contact@cyrilvincent.com>
2020-11-04 16:33:23 +0100

    ATP

--- tp_mesure.py
+++ tp_mesure.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("data/mesures/mesures.csv")
+plt.subplot(211)
 plt.plot(df.vm)
+plt.subplot(212)
+plt.plot(np.abs(df.vm - df.vt))
 plt.show()
 
-# vt = volt theorique
 noise = 1
-# Afficher la courbe |vt - vm|
-# Filtrer les incidents |vt - vm| > noise et les afficher
+df_errors = df[np.abs(df.vm - df.vt) > noise]
+print(df_errors)
