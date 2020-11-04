bc7f9651ed47dc8ff2886aaa821b7da7ea70aa16
cyrilvincent <contact@cyrilvincent.com>
2020-11-04 15:40:59 +0100

    ATP

--- tp_house.py
+++ tp_house.py
@@ -8,17 +8,15 @@ with open("data/house/house.csv") as f:
     loyers = np.array([float(row["loyer"]) for row in reader])
     surfaces = np.array([float(row["surface"]) for row in reader])
 
-
-
 surfaces_filtre = surfaces[surfaces < 180.0]
 loyers_filtre = loyers[surfaces < 180.0]
 
 plt.scatter(surfaces_filtre, loyers_filtre)
 
-slope, intercept, rvalue, pvalue, stderr = stats.linregress(surfaces_filtre, loyers_filtre)
-print(slope, intercept, rvalue, pvalue, stderr)
+res = stats.linregress(surfaces_filtre, loyers_filtre)
+print(res[0], res[1], res[2], res[3])
 
-f = lambda x : slope * x + intercept
-plt.plot(range(180), f(range(180)), color="red")
+f = lambda x : res[0] * x + res[1]
+plt.plot(surfaces_filtre, surfaces_filtre * res[0] + res[1], color="red")
 
 plt.show()
