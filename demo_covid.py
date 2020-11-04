0267f461993808807cf54159042c181d5e90677e
cyrilvincent <contact@cyrilvincent.com>
2020-11-04 11:36:24 +0100

    ATP

--- demo_covid.py
+++ demo_covid.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 with open("data/covid-france.txt") as f:
     reader = csv.DictReader(f)
@@ -7,3 +8,14 @@ with open("data/covid-france.txt") as f:
     for row in reader:
         letality = float(row["DC"]) / float(row["NbCas"])
         print(letality)
+
+with open("data/covid-france.txt") as f:
+    reader = list(csv.DictReader(f))
+    nbcas = [float(row["NbCas"]) for row in reader]
+    dc = [float(row["DC"]) for row in reader]
+plt.subplot(2,1,1)
+plt.plot(range(len(nbcas)), nbcas)
+plt.subplot(2,1,2)
+plt.plot(range(len(dc)), dc)
+#plt.yscale("log")
+plt.show()
---
+++ tp_house.py
@@ -0,0 +1,3 @@
+#Lire le fichiers
+# Créer la liste loyers, surfaces avec des liste en intention
+# Afficher le nuage de point (surface, loyer)
