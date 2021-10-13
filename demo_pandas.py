62f647a05231617bb10d9640eab48a2c608a4133
cyrilvincent <contact@cyrilvincent.com>
2021-10-13 14:46:16 +0200

    Pollen

--- demo_pandas.py
+++ demo_pandas.py
@@ -1,8 +1,20 @@
 import pandas as pd
-# pip install xlrd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+# pip install openpyxl
 
 dataframe = pd.read_excel("data/house/house.xlsx")
 dataframe = dataframe[dataframe.surface < 200]
 dataframe.to_csv("data/house/filtered.csv", index=None)
-print(dataframe)
-print(dataframe.surface)
+print(dataframe.describe())
+
+slope, intercept, rvalue,_,_ = stats.linregress(dataframe.surface, dataframe.loyer)
+print(f"Correlation: {rvalue*100:.1f}%")
+
+f = lambda x: slope * x + intercept
+
+plt.scatter(dataframe.surface, dataframe.loyer)
+x = np.arange(200)
+plt.plot(x, f(x), color="red")
+plt.show()
