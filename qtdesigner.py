bcf250b29bb5d82a4a5112680bdc9631a14133da
Cyril Vincent <contact@cyrilvincent.com>
2023-11-28 14:38:09 +0100

    ST

--- qtdesigner.py
+++ qtdesigner.py
@@ -5,8 +5,12 @@ import sys
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('ui/form.ui', self)
+        uic.loadUi('ui/main.ui', self)
+        self.pushButton.clicked.connect(self.pushButton_clicked)
         self.show()
+
+    def pushButton_clicked(self):
+        self.result_label.setText("TOTO")
 
 
 app = QtWidgets.QApplication(sys.argv)
