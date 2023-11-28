8ab245c4f634c19531bc436ceeda5a875318ec3a
Cyril Vincent <contact@cyrilvincent.com>
2023-11-28 13:55:48 +0100

    ST

--- tp_widget.py
+++ tp_widget.py
@@ -1,10 +1,42 @@
-# Créer une app
-# LineEdit
-# Checkbox degre ou radian
-# Button Ok
-# Label
-# On saisie un chiffre dans lineedit (convertir float())
-# Si on appuie sur Ok affiche le sin dans label math.sin en radian par défaut
-# Si checbox est checkée => en degres (math.radians(30))
-# Bonus Afficher en rouge un label d'erreur si ce qui est saisie != float
-# Correction à 13h45
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton, QCheckBox
+import math
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.input = QLineEdit()
+        self.checkbox = QCheckBox("Degrees")
+        self.button = QPushButton("OK")
+        self.button.clicked.connect(self.button_clicked)
+        self.label = QLabel()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.checkbox)
+        layout.addWidget(self.button)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(container)
+
+    def button_clicked(self):
+        value = float(self.input.text())
+        if self.checkbox.isChecked():
+            value = math.radians(value)
+        result = math.sin(value)
+        self.label.setText(f"{result:.2f}")
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
