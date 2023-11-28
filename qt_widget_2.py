96eb139af643bc88c86cc44b7cf795a9d0c2402f
Cyril Vincent <contact@cyrilvincent.com>
2023-11-28 12:11:33 +0100

    ST

---
+++ qt_widget_2.py
@@ -0,0 +1,46 @@
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+)
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = QLabel("Hello")
+        font = widget.font()
+        font.setPointSize(30)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+
+        self.setCentralWidget(widget)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+app.exec()
---
+++ tp_widget.py
@@ -0,0 +1,8 @@
+# Créer une app
+# LineEdit
+# Checkbox degre ou radian
+# Button Ok
+# Label
+# On saisie un chiffre dans lineedit (convertir float())
+# Si on appuie sur Ok affiche le sin dans label math.sin en radian par défaut
+# Si checbox est checkée => en degres (math.radians(30))
