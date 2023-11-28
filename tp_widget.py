ac8bccbe6cb3130005a1eb6a2aaea8474ff9334b
Cyril Vincent <contact@cyrilvincent.com>
2023-11-28 12:13:32 +0100

    ST

--- tp_widget.py
+++ tp_widget.py
@@ -6,3 +6,5 @@
 # On saisie un chiffre dans lineedit (convertir float())
 # Si on appuie sur Ok affiche le sin dans label math.sin en radian par défaut
 # Si checbox est checkée => en degres (math.radians(30))
+# Bonus Afficher en rouge un label d'erreur si ce qui est saisie != float
+# Correction à 13h45
