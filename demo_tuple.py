7e95a718d10a381ff8c8990b051acb945a221b55
Cyril Vincent <contact@cyrilvincent.com>
2023-12-11 13:51:56 +0100

    ST

--- demo_tuple.py
+++ demo_tuple.py
@@ -13,7 +13,24 @@ def min_max_avg(l: List[float]) -> Tuple[float, float, float]:
             min = i
     return min, max, sum / len(l)
 
+def min_max_avg_dico(l: List[float]) -> Tuple[float, float, float]:
+    sum = l[0]
+    min = l[0]
+    max = l[0]
+    for i in l[1:]:
+        sum += i
+        if i > max:
+            max = i
+        if i < min:
+            min = i
+    return {
+        "min": min,
+        "max": max,
+        "avg": sum / len(l)
+    }
+
 l = range(10)
 min, max, avg = min_max_avg(l)
 print(min, max, avg)
-
+res = min_max_avg_dico(l)
+print(res["min"], res["max"], res["avg"])
