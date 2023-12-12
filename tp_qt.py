26c7a7d722f215fb6feec8ee6c384386d2f4efcb
Cyril Vincent <contact@cyrilvincent.com>
2023-12-12 15:31:15 +0100

    ST

---
+++ tp_qt.py
@@ -0,0 +1,5 @@
+# Qt avec un lineEdit + Button + label
+# Saisir un chiffre dans lineEdit
+# Quand on appuie sur le button affiche le sin dans le label : float() math.sin(radian)
+# Bonus : Gérer les exceptions : try except : 2ème label : affiche l'erreur
+# Bonus : QCheckBox if checked => radians sinon en degres : math.radians()
