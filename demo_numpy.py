ccc5fa231a5e31010fde15a433775d638dc743d2
Cyril Vincent <contact@cyrilvincent.com>
2023-12-13 09:52:29 +0100

    ST

---
+++ demo_numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+v1 = np.arange(-np.pi, np.pi, 0.01)
+print(v1)
+v2 = np.sin(v1)
+
+# plt.plot(v1, v2)
+# plt.show()
+
+v3 = np.array([1,2,3,4,5])
+v4 = np.arange(5)
+print(v3 * v1)
+
---
+++ tp_house.py
@@ -0,0 +1,9 @@
+# Charger house.csv en pandas
+# Créer la colonne loyer_per_m2
+# np.mean = moyenne
+# np.std = ecart type
+# np.median = medianne
+# Faire la fonction mean_std_median(surface) => Tuple[float, float, float*] => (moyenne, std, mediane)
+# Bonus1 : rendre ce code OO
+# Bonus2 : doc scipy.linregress : calculer la regression lineaire
+# Bonus3 : afficher la regression lineaire
