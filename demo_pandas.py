20ac9b465b897269827808a973a831aaa83e5693
Cyril Vincent <contact@cyrilvincent.com>
2023-12-13 09:27:17 +0100

    ST

--- demo_pandas.py
+++ demo_pandas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 df = pd.read_csv("data/house/house.csv")
 df = df[df.surface < 200]
@@ -10,5 +11,9 @@ print(df.head())
 print(df.describe())
 df.to_json("data/house/house.json")
 
+fsin = [math.fabs(math.sin(x / 100) * 8000) for x in range(0 ,628)]
+plt.subplot(1,2,1)
+plt.plot(range(0,628), fsin, "red")
+plt.subplot(1,2,2)
 plt.scatter(df.surface, df.loyer)
 plt.show()
