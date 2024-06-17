0b42d5707e64dc1ec48487618d9ae814d2bf4297
cyrilvincent <contact@cyrilvincent.com>
2024-06-17 11:04:30 +0200

    ST

--- demo_tuple.py
+++ demo_tuple.py
@@ -4,11 +4,24 @@ def min_max_avg(l: list[float]) -> tuple[float, float, float]:
     :param l:
     :return:
     """
-    return 0., 10., 5.
+    min = l[0]
+    max = l[0]
+    sum = 0
+    for val in l:
+        sum += val
+        if val < min:
+            min = val
+        if val > max:
+            max = val
+    return min, max, sum / len(l)
+
 
 if __name__ == '__main__':
-    min, max, avg = min_max_avg([1,2,3])
+    min, max, avg = min_max_avg(range(10))
     print(min, max, avg)
     v = range(220,230,1)
     a = range(10, 20, 1)
     # calculer p = a * v sans numpy (for + zip)
+    p = [volt * amp for volt, amp in zip(v, a)]
+    p = [res[0] * res[1] for res in zip(v, a)]
+    print(p)
