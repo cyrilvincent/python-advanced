a20b1ae671e167e717f6db04713298033c404a7d
cyrilvincent <contact@cyrilvincent.com>
2024-06-18 15:03:45 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -11,14 +11,14 @@ class AbstractRepository(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def load(self, path): ...
 
-    # @abc.abstractmethod
-    # def filter(self, surface: float): ...
-    #
-    # @abc.abstractmethod
-    # def loyer_per_m2(self) -> list[float]: ...
-    #
-    # @abc.abstractmethod
-    # def loyer_per_m2_mean(self) -> float: ...
+    @abc.abstractmethod
+    def filter(self, surface: float): ...
+
+    @abc.abstractmethod
+    def loyer_per_m2(self) -> list[float]: ...
+
+    @abc.abstractmethod
+    def loyer_per_m2_mean(self, loyer_per_m2) -> float: ...
 
 
 class HouseRepository(AbstractRepository):
@@ -34,4 +34,30 @@ class HouseRepository(AbstractRepository):
                 loyer = int(row["loyer"])
                 self.surfaces.append(surface)
                 self.loyers.append(loyer)
+
+    def filter(self, surface):
+        loyers = []
+        surfaces = []
+        for i in range(len(self.surfaces)):
+            if self.surfaces[i] < surface:
+                loyers.append(self.loyers[i])
+                surfaces.append(self.surfaces[i])
+        self.loyers = loyers
+        self.surfaces = surfaces
+
+    def filter(self, surface_max):
+        res = ((loyer, surface) for loyer, surface in zip(self.loyers, self.surfaces) if surface < surface_max)
+        self.loyers = (loyer for loyer, surface in res)
+        self.surfaces = (surface for loyer, surface in res)
+
+    def loyer_per_m2(self) -> list[float]:
+        res = (loyer / surface for loyer, surface in zip(self.loyers, self.surfaces))
+        return res
+
+    def loyer_per_m2_mean(self, loyer_per_m2) -> float:
+        return sum(loyer_per_m2) / len(loyer_per_m2)
+
+
+
+
 
