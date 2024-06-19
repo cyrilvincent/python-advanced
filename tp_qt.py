a6ec2064cf34f4239a1f227c7e6316ad87a5eb1c
cyrilvincent <contact@cyrilvincent.com>
2024-06-19 15:07:23 +0200

    CEA

--- tp_qt.py
+++ tp_qt.py
@@ -1,7 +1,8 @@
 import sys
 
-from PyQt6.QtWidgets import QMainWindow, QFileDialog
+from PyQt6.QtWidgets import QMainWindow, QFileDialog, QWidget
 from ui.search_window import Ui_MainWindow
+from ui.detail_window import Ui_Form
 from PyQt6 import QtWidgets
 import media
 import config
@@ -13,16 +14,41 @@ class MyApp(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.pushButton_clicked)
+        self.listWidget.currentItemChanged.connect(self.listWidget_currentItemChanged)
         self.service = media.BookService()
         self.service.load(config.csv_path)
         self.radioTitleButton.setChecked(True)
+        self.res = []
         self.show()
 
     def pushButton_clicked(self):
         text = self.lineEdit.text()
-        res = self.service.search_by_title(text.strip())
-        for book in res:
+        self.res = self.service.search_by_title(text.strip())
+        for book in self.res:
             self.listWidget.addItem(book.title)
+
+    def listWidget_currentItemChanged(self):
+        book = self.res[self.listWidget.currentRow()]
+        detail_window = DetailWindow(book)
+        detail_window.show()
+        print("OK")
+
+
+
+class DetailWindow(QWidget, Ui_Form):
+
+    def __init__(self, book: media.Book):
+        super().__init__()
+        self.setupUi(self)
+        self.book = book
+        self.pushButton.clicked.connect(self.saveButton_clicked)
+        self.idLineEdit.setText(book.ean)
+        self.titleLineEdit.setText(book.title)
+        self.priceLineEdit.setText(f"{book.net_price():.2f} €")
+
+    def saveButton_clicked(self):
+        print("Save")
+
 
 
 
